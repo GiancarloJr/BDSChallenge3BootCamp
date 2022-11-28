5e48f44861be7aefc6bd1a534bdcc7e87b8ead1d
GiancarloJr <giancarlo456@hotmail.com>
2022-11-28 19:42:38 -0300

    Resource and Service

---
+++ backend/src/main/java/com/devsuperior/movieflix/resources/ReviewResources.java
@@ -0,0 +1,27 @@
+package com.devsuperior.movieflix.resources;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.services.ReviewServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequestMapping("/reviews")
+@RestController
+public class ReviewResources {
+
+    @Autowired
+    private ReviewServices reviewServices;
+
+    @PostMapping
+    public ResponseEntity<ReviewDTO> insert(@Valid @RequestBody ReviewDTO reviewDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewServices.insert(reviewDTO));
+    }
+}
---
+++ backend/src/main/java/com/devsuperior/movieflix/services/ReviewServices.java
@@ -0,0 +1,37 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServices {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private AuthService authService;
+
+
+    public ReviewDTO insert(ReviewDTO reviewDTO){
+        Review entity = new Review();
+        entity.setId(reviewDTO.getId());
+        entity.setText(reviewDTO.getText());
+        entity.setMovie(movieRepository.getOne(reviewDTO.getMovieId()));
+        entity.setUser(authService.authenticated());
+        return new ReviewDTO(reviewRepository.save(entity));
+    }
+
+//    public Review DtoToEntity(ReviewDTO reviewDTO){
+//        Review entity = new Review();
+//        entity.setId(reviewDTO.);
+//    }
+}
