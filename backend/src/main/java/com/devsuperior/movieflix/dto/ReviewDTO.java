888e021cf574a550c23cc74029f2001352c02394
GiancarloJr <giancarlo456@hotmail.com>
2022-11-28 19:42:28 -0300

    Entity and DTO

--- backend/src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
+++ backend/src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -1,19 +1,51 @@
 package com.devsuperior.movieflix.dto;
 
+
 import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class ReviewDTO {
 
+    private Long id;
+
+    @NotBlank
     private String Text;
+
+    @NotNull
     private Long movieId;
 
-    public ReviewDTO(){
+    private UserDTO user;
 
+
+    public ReviewDTO(){
     }
 
-    public ReviewDTO(String text, Long movieId) {
-        Text = text;
+    public ReviewDTO(Long id, String text, Long movieId, UserDTO user) {
+        this.id = id;
+        this.Text = text;
         this.movieId = movieId;
+        this.user = user;
+    }
+
+    public ReviewDTO(Review review) {
+        this.id = review.getId();
+        this.Text = review.getText();
+        this.movieId = review.getMovie().getId();
+        this.user = new UserDTO(review.getUser());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getText() {
@@ -30,5 +62,13 @@ public class ReviewDTO {
 
     public void setMovieId(Long movieId) {
         this.movieId = movieId;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
     }
 }
